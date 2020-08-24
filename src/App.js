42ed486e1643dc5398df75d5298219e308b2d213
JiiVe3 <janne.vehniainen@gmail.com>
2020-08-24 13:42:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [ result, setResult ] = useState('');
+  const [ numberOne, setNumberOne ] = useState(0);
+  const [ numberTwo, setNumberTwo ] = useState(0);
+  const [ limit, setLimit ] = useState(0);
+
+  const style = {
+    wordWrap: 'break-word',
+    width: '90%'
+  }
+
+  const calculate = () => {
+    let resString = '';
+    let number = 0;
+    let jaannos = numberOne;
+    let isDone;
+    let i = 0;
+
+    while(!isDone) {
+      if (jaannos - numberTwo >= 0) {
+        jaannos -= numberTwo;
+        number++;
+        isDone = false;
+      } else {
+        isDone = true;
+      }
+    }
+    resString = number.toString()
+    setResult(resString);
+
+    if (jaannos > 0) {
+      if(limit > 0) {
+        resString = resString + ',';
+        setResult(resString);
+      }
+      while(i < limit) {
+        jaannos = jaannos * 10;
+        isDone = false;
+        number = 0;
+        while(!isDone) {
+          if (jaannos - numberTwo >= 0) {
+            jaannos -= numberTwo;
+            number++;
+            isDone = false;
+          } else {
+            resString = resString + number.toString();
+            setResult(resString);
+            isDone = true;
+          }
+        }
+        if (jaannos === 0) break;
+        i++;
+      }
+    }
+
+    // if (!(jaannos === 0)) {
+    //   jaannos = jaannos * 10;
+    //   isDone = false;
+    //   number = 0;
+    //   while(!isDone) {
+    //     if (jaannos - numberTwo >= 0) {
+    //       jaannos -= numberTwo;
+    //       number++;
+    //       isDone = false;
+    //     } else {
+    //       if (number >= 5) {
+    //         resString = resString.slice(0, -1) + (parseInt(resString.slice(-1)) + 1).toString();
+    //         setResult(resString);
+    //         isDone = true;
+    //       }
+    //     }
+    //   }
+    // }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={numberOne} onChange={(e) => setNumberOne(e.target.value)} />/<input value={numberTwo} onChange={(e) => setNumberTwo(e.target.value)} />
+      <button onClick={calculate}>divide</button><br/>
+      limit <input value={limit} onChange={(e) => setLimit(e.target.value)} />
+      <p style={style}>{result}</p>
     </div>
   );
 }
